f8cf2fb9d01802a459bfcd9b2c7d3fb9571c53ad
Greg Neagle <gregneagle@mac.com>
2010-08-26 15:03:17 +0000

    MSU.app: munki.py cleanup to make pylint happier.
    git-svn-id: http://munki.googlecode.com/svn/trunk@691 a4e17f2e-e282-11dd-95e1-755cbddbdd66

--- code/Managed Software Update/munki.py
+++ code/Managed Software Update/munki.py
@@ -18,23 +18,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+'''munki-specific code for use with Managed Software Update'''
+
 import os
 import subprocess
 import FoundationPlist
 
-_updatechecklaunchfile = "/private/tmp/.com.googlecode.munki.updatecheck.launchd"
+UPDATECHECKLAUNCHFILE = \
+    "/private/tmp/.com.googlecode.munki.updatecheck.launchd"
 
 def call(cmd):
-    # convenience function; works around an issue with subprocess.call
-    # in PyObjC in Snow Leopard
-    p = subprocess.Popen(cmd, bufsize=1, stdout=subprocess.PIPE, 
+    '''Convenience function; works around an issue with subprocess.call
+    in PyObjC in Snow Leopard'''
+    proc = subprocess.Popen(cmd, bufsize=1, stdout=subprocess.PIPE, 
                                 stderr=subprocess.PIPE)
-    (output, err) = p.communicate()
-    return p.returncode
+    (output, err) = proc.communicate()
+    return proc.returncode
 
 
 def getManagedInstallsPrefs():
-    # define default values
+    '''Define default preference values; 
+    Read preference values from ManagedInstalls.plist if it exists.'''
+    
     prefs = {}
     prefs['ManagedInstallDir'] = "/Library/Managed Installs"
     prefs['InstallAppleSoftwareUpdates'] = False
@@ -42,79 +47,86 @@ def getManagedInstallsPrefs():
     prefs['InstallRequiresLogout'] = False
         
     prefsfile = "/Library/Preferences/ManagedInstalls.plist"
-    pl = {}
     if os.path.exists(prefsfile):
         try:
-            pl = FoundationPlist.readPlist(prefsfile)
+            plist = FoundationPlist.readPlist(prefsfile)
         except FoundationPlist.NSPropertyListSerializationException:
-            pass
+            return prefs
         try:
-            for key in pl.keys():
-                if type(pl[key]).__name__ == "__NSCFDate":
+            for key in plist.keys():
+                if type(plist[key]).__name__ == "__NSCFDate":
                     # convert NSDate/CFDates to strings
-                    prefs[key] = str(pl[key])
+                    prefs[key] = str(plist[key])
                 else:
-                    prefs[key] = pl[key]
+                    prefs[key] = plist[key]
         except AttributeError:
             pass
                         
     return prefs
     
 def writeSelfServiceManifest(optional_install_choices):
+    '''Write out our self-serve manifest 
+    so managedsoftwareupdate can use it'''
     usermanifest = "/Users/Shared/.SelfServeManifest"
     try:
         FoundationPlist.writePlist(optional_install_choices, usermanifest)
-    except:
+    except FoundationPlist.FoundationPlistException:
         pass
     
 def getRemovalDetailPrefs():
+    '''Returns preference to control display of removal detail'''
     return getManagedInstallsPrefs().get('ShowRemovalDetail', False)
     
 def installRequiresLogout():
+    '''Returns preference to force logout for all installs'''
     return getManagedInstallsPrefs().get('InstallRequiresLogout', False)
 
 
 def getInstallInfo():
+    '''Returns the dictionary describing the managed installs and removals'''
     prefs = getManagedInstallsPrefs()
     managedinstallbase = prefs['ManagedInstallDir']
-    pl = {}
+    plist = {}
     installinfo = os.path.join(managedinstallbase, 'InstallInfo.plist')
     if os.path.exists(installinfo):
         try:
-            pl = FoundationPlist.readPlist(installinfo)
+            plist = FoundationPlist.readPlist(installinfo)
         except FoundationPlist.NSPropertyListSerializationException:
             pass
-    return pl
+    return plist
     
 
 def startUpdateCheck():
-    # does launchd magic to run managedsoftwareupdate as root
-    result = call(["/usr/bin/touch", _updatechecklaunchfile])
+    '''Does launchd magic to run managedsoftwareupdate as root.'''
+    result = call(["/usr/bin/touch", UPDATECHECKLAUNCHFILE])
     return result
     
 
 def getAppleUpdates():
+    '''Returns any available Apple updates'''
     prefs = getManagedInstallsPrefs()
     managedinstallbase = prefs['ManagedInstallDir']
-    pl = {}
+    plist = {}
     appleUpdatesFile = os.path.join(managedinstallbase, 'AppleUpdates.plist')
-    if os.path.exists(appleUpdatesFile) and prefs['InstallAppleSoftwareUpdates']:
+    if (os.path.exists(appleUpdatesFile) and 
+            prefs['InstallAppleSoftwareUpdates']):
         try:
-            pl = FoundationPlist.readPlist(appleUpdatesFile)
+            plist = FoundationPlist.readPlist(appleUpdatesFile)
         except FoundationPlist.NSPropertyListSerializationException:
             pass
-    return pl
+    return plist
     
 def humanReadable(kbytes):
     """Returns sizes in human-readable units."""
-    units = [(" KB",2**10), (" MB",2**20), (" GB",2**30), (" TB",2**40)] 
+    units = [(" KB", 2**10), (" MB", 2**20), (" GB", 2**30), (" TB", 2**40)] 
     for suffix, limit in units:
         if kbytes > limit:
             continue
         else:
-            return str(round(kbytes/float(limit/2**10),1))+suffix
+            return str(round(kbytes/float(limit/2**10), 1)) + suffix
     
-def trimVersionString(versString,tupleCount):
+def trimVersionString(versString, tupleCount):
+    '''Trims the version string to no more than tupleCount parts'''
     if versString == None or versString == "":
         return ""
     components = str(versString).split(".")
@@ -125,10 +137,11 @@ def trimVersionString(versString,tupleCount):
 def currentGUIusers():
     '''Gets a list of GUI users by parsing the output of /usr/bin/who'''
     gui_users = []
-    p = subprocess.Popen("/usr/bin/who", shell=False, stdin=subprocess.PIPE, 
-                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    (output, err) = p.communicate()
-    lines = output.splitlines()
+    proc = subprocess.Popen("/usr/bin/who", shell=False, 
+                            stdin=subprocess.PIPE, 
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    (output, err) = proc.communicate()
+    lines = str(output).splitlines()
     for line in lines:
         if "console" in line:
             parts = line.split()
@@ -137,15 +150,15 @@ def currentGUIusers():
     return gui_users
     
 def logoutNow():
-    # uses oscascript to run an AppleScript
-    # to tell loginwindow to logout
-    # ugly, but it works.
+    '''Uses oscascript to run an AppleScript
+    to tell loginwindow to logout.
+    Ugly, but it works.''' 
     
     script = """
 ignoring application responses
-	tell application "loginwindow"
-		«event aevtrlgo»
-	end tell
+    tell application "loginwindow"
+        «event aevtrlgo»
+    end tell
 end ignoring
 """
     cmd = ["/usr/bin/osascript"]
@@ -156,12 +169,12 @@ end ignoring
             cmd.append(line)
     result = call(cmd)
 
-    
 
 def logoutAndUpdate():
-    # touch a flag so the process that runs after
-	# logout knows it's OK to install everything
-    cmd = ["/usr/bin/touch",  "/private/tmp/com.googlecode.munki.installatlogout"]
+    '''Touch a flag so the process that runs after
+    logout knows it's OK to install everything'''
+    cmd = ["/usr/bin/touch", 
+           "/private/tmp/com.googlecode.munki.installatlogout"]
     result = call(cmd)
     if result == 0:
         logoutNow()
@@ -170,10 +183,12 @@ def logoutAndUpdate():
 
 
 def justUpdate():
-    # trigger managedinstaller via launchd KeepAlive path trigger
-    # we touch a file that launchd is is watching
-    # launchd, in turn, launches managedsoftwareupdate --installwithnologout as root
-    cmd = ["/usr/bin/touch",  "/private/tmp/.com.googlecode.munki.managedinstall.launchd"]
+    '''Trigger managedinstaller via launchd KeepAlive path trigger
+    We touch a file that launchd is is watching
+    launchd, in turn, 
+    launches managedsoftwareupdate --installwithnologout as root'''
+    cmd = ["/usr/bin/touch", 
+           "/private/tmp/.com.googlecode.munki.managedinstall.launchd"]
     return call(cmd)
         
 
