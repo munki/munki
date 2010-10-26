aa14ee03a68604cb752d484122d1bee902079708
Greg Neagle <gregneagle@mac.com>
2010-10-26 23:43:28 +0000

    Fix copy of svnrev to tmp root /usr/local/munki/munkilib/svnversion
    git-svn-id: http://munki.googlecode.com/svn/trunk@873 a4e17f2e-e282-11dd-95e1-755cbddbdd66

--- code/tools/make_munki_pkg.sh
+++ code/tools/make_munki_pkg.sh
@@ -129,7 +129,7 @@ if [ "$BUILD_USR" == "YES" ]; then
     cp "$munkiroot/code/client/"* ./usr/local/munki/ 2>&1 | grep -v "munkilib is a directory"
     cp "$munkiroot"/code/client/munkilib/*.py ./usr/local/munki/munkilib/
     cp "$munkiroot"/code/client/munkilib/version.plist ./usr/local/munki/munkilib/
-    echo $svnrev > ./code/client/munkilib/svnversion
+    echo $svnrev > ./usr/local/munki/munkilib/svnversion
     # no pre/postflight scripts in the package, please
     rm -f ./usr/local/munki/preflight
     rm -f ./usr/local/munki/postflight
