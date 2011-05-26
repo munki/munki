b875db5dab734d9ba22ace4ff67f12ca5c1cac0e
Greg Neagle <gregneagle@mac.com>
2011-05-26 23:02:30 +0000

    Updated uninstall_munki.sh script to reference new receipt names.
    git-svn-id: http://munki.googlecode.com/svn/trunk@1203 a4e17f2e-e282-11dd-95e1-755cbddbdd66

--- code/tools/uninstall_munki.sh
+++ code/tools/uninstall_munki.sh
@@ -14,7 +14,8 @@ rm -f /Library/LaunchDaemons/com.googlecode.munki.*
 rm -f /Library/LaunchAgents/com.googlecode.munki.*
 rm -rf "/Library/Managed Installs"
 rm -rf /usr/local/munki
-pkgutil --forget com.googlecode.munki.usr
+pkgutil --forget com.googlecode.munki.core
+pkgutil --forget com.googlecode.munki.admin
 pkgutil --forget com.googlecode.munki.app
-pkgutil --forget com.googlecode.munki.lib
+pkgutil --forget com.googlecode.munki.launchd
 pkgutil --forget com.googlecode.munki
