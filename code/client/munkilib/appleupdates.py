munki/munki

commit e22995c5c49e39ea989a2d0109aa9a030388fab3

Remove duplicate os.path.exists() calls when checking for real_cache_dir existence.

code/client/munkilib/appleupdates.py
@@ -122,10 +122,11 @@ class AppleUpdates(object):
         self._managed_install_dir = munkicommon.pref('ManagedInstallDir')
         
         real_cache_dir = os.path.join(self._managed_install_dir, 'swupd')
-        if os.path.exists(real_cache_dir) and not os.path.isdir(real_cache_dir):
-            munki_common.display_error(
-                '%s exists but is not a dir.', real_cache_dir)
-        elif not os.path.exists(real_cache_dir):
+        if os.path.exists(real_cache_dir):
+            if not os.path.isdir(real_cache_dir):
+                munki_common.display_error(
+                    '%s exists but is not a dir.', real_cache_dir)
+        else:
             os.mkdir(real_cache_dir)
 
         # symlink to work around an issue with paths containing spaces

commit 22002c715330c78c19b7974e349deb453b6cd7c1

Fix s/munki_common/munkicommon/ typo introduced in e22995c5c49e

code/client/munkilib/appleupdates.py
@@ -124,7 +124,7 @@ class AppleUpdates(object):
         real_cache_dir = os.path.join(self._managed_install_dir, 'swupd')
         if os.path.exists(real_cache_dir):
             if not os.path.isdir(real_cache_dir):
-                munki_common.display_error(
+                munkicommon.display_error(
                     '%s exists but is not a dir.', real_cache_dir)
         else:
             os.mkdir(real_cache_dir)