b24b7814b0b20baf08ab368e14ab9ab71f1d8f66
Greg Neagle <gregneagle@mac.com>
2014-06-19 08:29:51 -0700

    Add RestartAction to available keys in optional_installs items so that MSC.app can display the info.

--- code/client/munkilib/updatecheck.py
+++ code/client/munkilib/updatecheck.py
@@ -1572,7 +1572,8 @@ def processOptionalInstall(manifestitem, cataloglist, installinfo):
     iteminfo['description'] = item_pl.get('description', '')
     iteminfo['version_to_install'] = item_pl.get('version', 'UNKNOWN')
     iteminfo['display_name'] = item_pl.get('display_name', '')
-    for key in ['category', 'developer', 'icon_name', 'requires']:
+    for key in ['category', 'developer', 'icon_name',
+                'requires', 'RestartAction']:
         if key in item_pl:
             iteminfo[key] = item_pl[key]
     iteminfo['installed'] = someVersionInstalled(item_pl)
@@ -1591,7 +1592,7 @@ def processOptionalInstall(manifestitem, cataloglist, installinfo):
                                warn=False):
             iteminfo['note'] = \
                 'Insufficient disk space to download and install.'
-
+   
     munkicommon.display_debug1(
         'Adding %s to the optional install list', iteminfo['name'])
     installinfo['optional_installs'].append(iteminfo)
