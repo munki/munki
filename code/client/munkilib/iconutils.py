842738da01b38ee3b1982dbb726386904415f28f
Greg Neagle <gregneagle@mac.com>
2014-06-27 10:40:05 -0700

    When using lsbom to read pathnames, decode from UTF-8 to unicode. Attempt to fix issue 341.

--- code/client/munkilib/iconutils.py
+++ code/client/munkilib/iconutils.py
@@ -127,8 +127,8 @@ def extractAppIconsFromFlatPkg(pkg_path):
         if proc.returncode:
             print >> sys.stderr, u'Could not lsbom %s' % bomfile
         # record paths to all app Info.plist files
-        pkg_dict[pkgname]= [
-            os.path.normpath(line) for line in output.splitlines()
+        pkg_dict[pkgname] = [os.path.normpath(line) 
+            for line in output.decode('utf-8').splitlines()
             if line.endswith(u'.app/Contents/Info.plist')]
         if not pkg_dict[pkgname]:
             # remove empty lists
