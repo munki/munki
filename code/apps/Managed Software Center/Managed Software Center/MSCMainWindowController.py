ac14ae82e324d6b22a177be0461e468488e89cec
Greg Neagle <gregneagle@mac.com>
2014-09-23 11:13:19 -0700

    Minor window tweaks

--- code/apps/Managed Software Center/Managed Software Center/MSCMainWindowController.py
+++ code/apps/Managed Software Center/Managed Software Center/MSCMainWindowController.py
@@ -139,14 +139,15 @@ class MSCMainWindowController(NSWindowController):
             self.updateNow()
             self.loadUpdatesPage_(self)
 
-    def window_willPositionSheet_usingRect_(self, window, sheet, rect):
-        '''NSWindowDelegate method that allows us to modify the 
-        position sheets appear attached to a window'''
-        # move the anchor point of our sheets to below our toolbar
-        # (or really, to the top of the web view)
-        webViewRect = self.webView.frame()
-        return NSMakeRect(webViewRect.origin.x, webViewRect.origin.y + webViewRect.size.height,
-                          webViewRect.size.width, 0)
+    ### no longer needed now that we are using a "real" NSToolbar ###
+    #def window_willPositionSheet_usingRect_(self, window, sheet, rect):
+    #    '''NSWindowDelegate method that allows us to modify the
+    #    position sheets appear attached to a window'''
+    #    # move the anchor point of our sheets to below our toolbar
+    #    # (or really, to the top of the web view)
+    #    webViewRect = self.webView.frame()
+    #    return NSMakeRect(webViewRect.origin.x, webViewRect.origin.y + webViewRect.size.height,
+    #                      webViewRect.size.width, 0)
 
     def loadInitialView(self):
         '''Called by app delegate from applicationDidFinishLaunching:'''
