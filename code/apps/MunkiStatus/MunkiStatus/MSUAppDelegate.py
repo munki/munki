e31733ba5abda74d10fe3743c81f42417e5c4821
Greg Neagle <gregneagle@mac.com>
2014-10-28 08:42:58 -0700

    PyLint cleanups

--- code/apps/MunkiStatus/MunkiStatus/MSUAppDelegate.py
+++ code/apps/MunkiStatus/MunkiStatus/MSUAppDelegate.py
@@ -16,22 +16,44 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+'''Following Cocoa application design pattern - defines our app delegate
+class'''
 
-import os
-
-from objc import YES, NO, IBAction, IBOutlet, nil
+from objc import YES, NO, IBOutlet
 import PyObjCTools
+# pylint: disable=wildcard-import
+# pylint: disable=unused-wildcard-import
+# pylint: disable=redefined-builtin
 from Foundation import *
 from AppKit import *
-
+# pylint: enable=redefined-builtin
+# pylint: enable=wildcard-import
 
 import munki
 
+# lots of camelCase names following Cocoa style
+# pylint: disable=C0103
+
+
 class MSUAppDelegate(NSObject):
-    
+    '''Implements some NSApplicationDelegateProtocol methods'''
+
+    # since this subclasses NSObject,
+    # it doesn't have a Python __init__method
+    # pylint: disable=no-init
+
+    # several delegate methods pass a 'sender' object that we don't use
+    # pylint: disable=unused-argument
+
     statusWindowController = IBOutlet()
-    
+
     def applicationWillFinishLaunching_(self, sender):
+        '''NSApplicationDelegate method
+        Sent by the default notification center immediately before the
+        application object is initialized.'''
+
+        # pylint: disable=no-self-use
+
         consoleuser = munki.getconsoleuser()
         if consoleuser == None or consoleuser == u"loginwindow":
             # don't show menu bar
@@ -40,10 +62,15 @@ class MSUAppDelegate(NSObject):
             NSApp.activateIgnoringOtherApps_(YES)
 
     def applicationDidFinishLaunching_(self, sender):
+        '''NSApplicationDelegate method
+        Sent by the default notification center after the application has
+        been launched and initialized but before it has received its first
+        event.'''
+
         # Prevent automatic relaunching at login on Lion+
         if NSApp.respondsToSelector_('disableRelaunchOnLogin'):
             NSApp.disableRelaunchOnLogin()
-        
+
         # show the default initial view
         self.statusWindowController.initStatusSession()
 
--- code/apps/MunkiStatus/MunkiStatus/MSUStatusWindowController.py
+++ code/apps/MunkiStatus/MunkiStatus/MSUStatusWindowController.py
@@ -16,22 +16,30 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+'''Controller for the main status window'''
 
 from objc import YES, NO, IBAction, IBOutlet, nil
 from PyObjCTools import AppHelper
 
 import os
 import munki
-import time
 import FoundationPlist
+
+# pylint: disable=wildcard-import
+# pylint: disable=unused-wildcard-import
+# pylint: disable=redefined-builtin
 from Foundation import *
 from AppKit import *
-from SystemConfiguration import SCDynamicStoreCopyConsoleUser
+# pylint: enable=redefined-builtin
+# pylint: enable=wildcard-import
+
+# lots of camelCase names, following Cocoa convention
+# pylint: disable=invalid-name
 
 debug = False
 
 def getLoginwindowPicture():
+    '''Returns the image behind the loginwindow (in < 10.7)'''
     desktopPicturePath = ''
     loginwindowPrefsPath = "/Library/Preferences/com.apple.loginwindow.plist"
     if os.path.exists(loginwindowPrefsPath):
@@ -40,25 +48,25 @@ def getLoginwindowPicture():
             desktopPicturePath = loginwindowPrefs.get('DesktopPicture', '')
             if desktopPicturePath:
                 if os.path.exists(desktopPicturePath):
-                    theImage = \
-                        NSImage.alloc().initWithContentsOfFile_(
-                            desktopPicturePath)
+                    theImage = NSImage.alloc().initWithContentsOfFile_(
+                        desktopPicturePath)
                     if theImage:
                         return theImage
                 return NSImage.imageNamed_("Solid Aqua Blue")
     theImage = NSImage.alloc().initWithContentsOfFile_(
-                        "/System/Library/CoreServices/DefaultDesktop.jpg")
+        "/System/Library/CoreServices/DefaultDesktop.jpg")
     if theImage:
         return theImage
     else:
         return NSImage.imageNamed_("Solid Aqua Blue")
-        
+
 
 class MSUStatusWindowController(NSObject):
-    '''
-    Controls the status window. This was formerly part of a
-    seperate application known as MunkiStatus.app
-    '''
+    '''Controls the status window.'''
+
+    # since this subclasses NSObject,
+    # it doesn't have a Python __init__method
+    # pylint: disable=no-init
 
     window = IBOutlet()
     messageFld = IBOutlet()
@@ -75,58 +83,71 @@ class MSUStatusWindowController(NSObject):
     got_status_update = False
     receiving_notifications = False
     timer = None
+    timeout_counter = 0
+    saw_process = False
+    managedsoftwareupdate_pid = None
 
     @IBAction
     def stopBtnClicked_(self, sender):
+        '''Called when stop button is clicked in the status window'''
         if debug:
             NSLog(u"Stop button was clicked.")
         sender.setState_(1)
         self.stopBtnState = 1
         sender.setEnabled_(False)
         # send a notification that stop button was clicked
-        STOP_REQUEST_FLAG = '/private/tmp/com.googlecode.munki.managedsoftwareupdate.stop_requested'
+        STOP_REQUEST_FLAG = ('/private/tmp/com.googlecode.munki.'
+                             'managedsoftwareupdate.stop_requested')
         if not os.path.exists(STOP_REQUEST_FLAG):
             open(STOP_REQUEST_FLAG, 'w').close()
-    
+
     def registerForNotifications(self):
         '''Register for notification messages'''
-        notification_center = NSDistributedNotificationCenter.defaultCenter()
-        notification_center.addObserver_selector_name_object_suspensionBehavior_(
+        dnc = NSDistributedNotificationCenter.defaultCenter()
+        dnc.addObserver_selector_name_object_suspensionBehavior_(
             self,
             self.updateStatus_,
             'com.googlecode.munki.managedsoftwareupdate.statusUpdate',
             None,
             NSNotificationSuspensionBehaviorDeliverImmediately)
-        notification_center.addObserver_selector_name_object_suspensionBehavior_(
+        dnc.addObserver_selector_name_object_suspensionBehavior_(
             self,
             self.managedsoftwareupdateStarted_,
             'com.googlecode.munki.managedsoftwareupdate.started',
             None,
             NSNotificationSuspensionBehaviorDeliverImmediately)
-        notification_center.addObserver_selector_name_object_suspensionBehavior_(
+        dnc.addObserver_selector_name_object_suspensionBehavior_(
             self,
             self.managedsoftwareupdateEnded_,
             'com.googlecode.munki.managedsoftwareupdate.ended',
             None,
             NSNotificationSuspensionBehaviorDeliverImmediately)
         self.receiving_notifications = True
-    
+
     def unregisterForNotifications(self):
-        '''Tell the DistributedNotificationCenter to stop sending us notifications'''
+        '''Tell the DistributedNotificationCenter to stop sending us
+        notifications'''
         NSDistributedNotificationCenter.defaultCenter().removeObserver_(self)
         # set self.receiving_notifications to False so our process monitoring
         # thread will exit
         self.receiving_notifications = False
-    
+
     def managedsoftwareupdateStarted_(self, notification):
+        '''Called when we get a
+        com.googlecode.munki.managedsoftwareupdate.started notification'''
         if 'pid' in notification.userInfo():
             self.managedsoftwareupdate_pid = notification.userInfo()['pid']
-            NSLog('managedsoftwareupdate pid %s started' % self.managedsoftwareupdate_pid)
+            NSLog('managedsoftwareupdate pid %s started'
+                  % self.managedsoftwareupdate_pid)
 
     def managedsoftwareupdateEnded_(self, notification):
-        NSLog('managedsoftwareupdate pid %s ended' % notification.userInfo().get('pid'))
+        '''Called when we get a
+        com.googlecode.munki.managedsoftwareupdate.ended notification'''
+        NSLog('managedsoftwareupdate pid %s ended'
+              % notification.userInfo().get('pid'))
 
     def initStatusSession(self):
+        '''Initialize our status session'''
         consoleuser = munki.getconsoleuser()
         if consoleuser == None or consoleuser == u"loginwindow":
             self.displayBackdropWindow()
@@ -158,19 +179,21 @@ class MSUStatusWindowController(NSObject):
             # process failure
             self.timeout_counter = 6
             self.saw_process = False
+            # pylint: disable=line-too-long
             self.timer = NSTimer.scheduledTimerWithTimeInterval_target_selector_userInfo_repeats_(
-                                                          5.0, self, self.checkProcess_, None, YES)
-            
-    def checkProcess_(self, timer):
+                5.0, self, self.checkProcess, None, YES)
+            # pylint: enable=line-too-long
+
+    def checkProcess(self):
         '''Monitors managedsoftwareupdate process for failure to start
         or unexpected exit, so we're not waiting around forever if
         managedsoftwareupdate isn't running.'''
         PYTHON_SCRIPT_NAME = 'managedsoftwareupdate'
         NEVER_STARTED = -2
         UNEXPECTEDLY_QUIT = -1
-        
+
         NSLog('checkProcess timer fired')
-        
+
         if self.got_status_update:
             # we got a status update since we last checked; no need to
             # check the process table
@@ -192,11 +215,13 @@ class MSUStatusWindowController(NSObject):
                 self.statusSessionFailed_(NEVER_STARTED)
 
     def statusSessionFailed_(self, sessionResult):
+        '''Called if the status session fails'''
         NSLog('statusSessionFailed: %s' % sessionResult)
         self.cleanUpStatusSession()
         NSApp.terminate_(self)
 
     def cleanUpStatusSession(self):
+        '''Clean things up before we exit'''
         self.unregisterForNotifications()
         if self.backdropWindow and self.backdropWindow.isVisible():
             self.backdropWindow.orderOut_(self)
@@ -207,12 +232,13 @@ class MSUStatusWindowController(NSObject):
             self.timer = None
 
     def displayBackdropWindow(self):
+        '''Draw a window that covers the login UI'''
         if self.backdropWindow:
             self.backdropWindow.setCanBecomeVisibleWithoutLogin_(True)
             self.backdropWindow.setLevel_(NSStatusWindowLevel)
             screenRect = NSScreen.mainScreen().frame()
             self.backdropWindow.setFrame_display_(screenRect, True)
-            
+
             darwin_vers = int(os.uname()[2].split('.')[0])
             if darwin_vers < 11:
                 if self.backdropImageFld:
@@ -220,9 +246,12 @@ class MSUStatusWindowController(NSObject):
                     self.backdropImageFld.setImage_(bgImage)
                     self.backdropWindow.orderFrontRegardless()
             else:
+                # Lion+
+                # draw a transparent/translucent window to prevent interaction
+                # with the login UI
                 self.backdropImageFld.setHidden_(True)
-                translucentColor = \
-                    NSColor.blackColor().colorWithAlphaComponent_(0.35)
+                translucentColor = NSColor.blackColor(
+                    ).colorWithAlphaComponent_(0.35)
                 self.backdropWindow.setBackgroundColor_(translucentColor)
                 self.backdropWindow.setOpaque_(False)
                 self.backdropWindow.setIgnoresMouseEvents_(False)
@@ -231,6 +260,10 @@ class MSUStatusWindowController(NSObject):
                 self.backdropWindow.animator().setAlphaValue_(1.0)
 
     def updateStatus_(self, notification):
+        '''Called when we get a
+        com.googlecode.munki.managedsoftwareupdate.statusUpdate notification;
+        update our status display with information from the notification'''
+
         self.got_status_update = True
         info = notification.userInfo()
         # explictly get keys from info object; PyObjC in Mountain Lion
@@ -252,7 +285,7 @@ class MSUStatusWindowController(NSObject):
                 self.enableStopButton()
             else:
                 self.disableStopButton()
-    
+
         command = info.get('command')
         if command == 'activate':
             self.window.orderFrontRegardless()
@@ -263,6 +296,7 @@ class MSUStatusWindowController(NSObject):
             NSApp.terminate_(self)
 
     def setPercentageDone_(self, percent):
+        '''Set progress indicator to display percent done'''
         if float(percent) < 0:
             if not self.progressIndicator.isIndeterminate():
                 self.progressIndicator.setIndeterminate_(True)
@@ -275,13 +309,18 @@ class MSUStatusWindowController(NSObject):
 
     @AppHelper.endSheetMethod
     def restartAlertDidEnd_returnCode_contextInfo_(
-                                        self, alert, returncode, contextinfo):
+            self, alert, returncode, contextinfo):
+        '''Called when restart alert is dismissed'''
+        # we don't use the returncode or contextinfo arguments
+        # pylint: disable=unused-argument
         self.restartAlertDismissed = 1
         munki.restartNow()
 
     def doRestartAlert(self):
+        '''Display a restart alert'''
         self.restartAlertDismissed = 0
-        alert = NSAlert.alertWithMessageText_defaultButton_alternateButton_otherButton_informativeTextWithFormat_(
+        # pylint: disable=line-too-long
+        nsa = NSAlert.alertWithMessageText_defaultButton_alternateButton_otherButton_informativeTextWithFormat_(
             NSLocalizedString(u"Restart Required", None),
             NSLocalizedString(u"Restart", None),
             nil,
@@ -289,79 +328,107 @@ class MSUStatusWindowController(NSObject):
             NSLocalizedString(
                 u"Software installed or removed requires a restart. "
                 "You will have a chance to save open documents.", None))
-        alert.beginSheetModalForWindow_modalDelegate_didEndSelector_contextInfo_(
-            self.window, self, self.restartAlertDidEnd_returnCode_contextInfo_, nil)
+        # pylint: enable=line-too-long
+        nsa.beginSheetModalForWindow_modalDelegate_didEndSelector_contextInfo_(
+            self.window, self, self.restartAlertDidEnd_returnCode_contextInfo_,
+            nil)
 
     def setMessage_(self, messageText):
+        '''Set the main status message'''
         messageText = NSBundle.mainBundle().localizedStringForKey_value_table_(
             messageText, messageText, None)
         self.messageFld.setStringValue_(messageText)
 
     def setDetail_(self, detailText):
+        '''Set the status detail text'''
         detailText = NSBundle.mainBundle().localizedStringForKey_value_table_(
             detailText, detailText, None)
         self.detailFld.setStringValue_(detailText)
 
     def getStopBtnState(self):
+        '''Return True if the stop button was clicked; False otherwise'''
         return self.stopBtnState
 
     def hideStopButton(self):
-       self.stopBtn.setHidden_(True)
+        '''Hide the stop button'''
+        self.stopBtn.setHidden_(True)
 
     def showStopButton(self):
+        '''Show the stop button'''
         self.stopBtn.setHidden_(False)
 
     def enableStopButton(self):
+        '''Enable the stop button'''
         self.stopBtn.setEnabled_(True)
 
     def disableStopButton(self):
+        '''Disable the stop button'''
         self.stopBtn.setEnabled_(False)
 
     def getRestartAlertDismissed(self):
+        '''Return True if the restart alert was dismissed; False otherwise'''
         return self.restartAlertDismissed
 
 
 def more_localized_strings():
-    '''Some strings that are sent to us from managedsoftwareupdate. By putting them here,
-    the localize.py script will add them to the en.lproj/Localizable.strings file so localizers
-    will be able to discover them'''
-    foo = NSLocalizedString(u"Starting...", "managedsoftwareupdate message")
-    foo = NSLocalizedString(u"Finishing...", "managedsoftwareupdate message")
-    foo = NSLocalizedString(u"Performing preflight tasks...", "managedsoftwareupdate message")
-    foo = NSLocalizedString(u"Performing postflight tasks...", "managedsoftwareupdate message")
+    '''Some strings that are sent to us from managedsoftwareupdate. By putting
+    them here, the localize.py script will add them to the
+    en.lproj/Localizable.strings file so localizers will be able to discover
+    them'''
+    dummy = NSLocalizedString(u"Starting...", "managedsoftwareupdate message")
+    dummy = NSLocalizedString(u"Finishing...", "managedsoftwareupdate message")
+    dummy = NSLocalizedString(u"Performing preflight tasks...",
+                              "managedsoftwareupdate message")
+    dummy = NSLocalizedString(u"Performing postflight tasks...",
+                              "managedsoftwareupdate message")
 
-    foo = NSLocalizedString(u"Checking for available updates...", "managedsoftwareupdate message")
-    foo = NSLocalizedString(u"Checking for additional changes...", "managedsoftwareupdate message")
-    foo = NSLocalizedString(u"Software installed or removed requires a restart.",
-                            "managedsoftwareupdate message")
-    foo = NSLocalizedString(u"Waiting for network...", "managedsoftwareupdate message")
-    foo = NSLocalizedString(u"Done.", "managedsoftwareupdate message")
+    dummy = NSLocalizedString(u"Checking for available updates...",
+                              "managedsoftwareupdate message")
+    dummy = NSLocalizedString(u"Checking for additional changes...",
+                              "managedsoftwareupdate message")
+    dummy = NSLocalizedString(
+        u"Software installed or removed requires a restart.",
+        "managedsoftwareupdate message")
+    dummy = NSLocalizedString(u"Waiting for network...",
+                              "managedsoftwareupdate message")
+    dummy = NSLocalizedString(u"Done.", "managedsoftwareupdate message")
 
-    foo = NSLocalizedString(u"Retrieving list of software for this machine...",
-                            "managedsoftwareupdate message")
-    foo = NSLocalizedString(u"Verifying package integrity...", "managedsoftwareupdate message")
+    dummy = NSLocalizedString(
+        u"Retrieving list of software for this machine...",
+        "managedsoftwareupdate message")
+    dummy = NSLocalizedString(u"Verifying package integrity...",
+                              "managedsoftwareupdate message")
 
-    foo = NSLocalizedString(u"The software was successfully installed.",
-                            "managedsoftwareupdate message")
+    dummy = NSLocalizedString(u"The software was successfully installed.",
+                              "managedsoftwareupdate message")
 
-    foo = NSLocalizedString(u"Gathering information on installed packages",
-                            "managedsoftwareupdate message")
-    foo = NSLocalizedString(u"Determining which filesystem items to remove",
-                            "managedsoftwareupdate message")
-    foo = NSLocalizedString(u"Removing receipt info", "managedsoftwareupdate message")
-    foo = NSLocalizedString(u"Nothing to remove.", "managedsoftwareupdate message")
-    foo = NSLocalizedString(u"Package removal complete.", "managedsoftwareupdate message")
+    dummy = NSLocalizedString(u"Gathering information on installed packages",
+                              "managedsoftwareupdate message")
+    dummy = NSLocalizedString(u"Determining which filesystem items to remove",
+                              "managedsoftwareupdate message")
+    dummy = NSLocalizedString(u"Removing receipt info",
+                              "managedsoftwareupdate message")
+    dummy = NSLocalizedString(u"Nothing to remove.",
+                              "managedsoftwareupdate message")
+    dummy = NSLocalizedString(u"Package removal complete.",
+                              "managedsoftwareupdate message")
 
-    foo = NSLocalizedString(u"Checking for available Apple Software Updates...",
-                            "managedsoftwareupdate message")
-    foo = NSLocalizedString(u"Checking Apple Software Update catalog...",
-                            "managedsoftwareupdate message")
-    foo = NSLocalizedString(u"Downloading available Apple Software Updates...",
-                            "managedsoftwareupdate message")
-    foo = NSLocalizedString(u"Installing available Apple Software Updates...",
-                            "managedsoftwareupdate message")
+    dummy = NSLocalizedString(
+        u"Checking for available Apple Software Updates...",
+        "managedsoftwareupdate message")
+    dummy = NSLocalizedString(u"Checking Apple Software Update catalog...",
+                              "managedsoftwareupdate message")
+    dummy = NSLocalizedString(
+        u"Downloading available Apple Software Updates...",
+        "managedsoftwareupdate message")
+    dummy = NSLocalizedString(u"Installing available Apple Software Updates...",
+                              "managedsoftwareupdate message")
 
-    foo = NSLocalizedString(u"Running Adobe Setup", "managedsoftwareupdate message")
-    foo = NSLocalizedString(u"Running Adobe Uninstall", "managedsoftwareupdate message")
-    foo = NSLocalizedString(u"Starting Adobe installer...", "managedsoftwareupdate message")
-    foo = NSLocalizedString(u"Running Adobe Patch Installer", "managedsoftwareupdate message")
+    dummy = NSLocalizedString(u"Running Adobe Setup",
+                              "managedsoftwareupdate message")
+    dummy = NSLocalizedString(u"Running Adobe Uninstall",
+                              "managedsoftwareupdate message")
+    dummy = NSLocalizedString(u"Starting Adobe installer...",
+                              "managedsoftwareupdate message")
+    dummy = NSLocalizedString(u"Running Adobe Patch Installer",
+                              "managedsoftwareupdate message")
