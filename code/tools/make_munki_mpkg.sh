917159f1ab1e82d617d8851273a281b8cf3773d7
Samuel Keeley <samuel@dropbox.com>
2015-07-31 06:32:40 -0700

    only create Managed Software Update.app symlink if it previously existed.

--- code/tools/make_munki_mpkg.sh
+++ code/tools/make_munki_mpkg.sh
@@ -174,7 +174,7 @@ echo "  metapackage version: $MPKGVERSION"
 echo
 
 # Build Managed Software Center.
-echo "Building Managed Software Update.xcodeproj..."
+echo "Building Managed Software Center.xcodeproj..."
 pushd "$MUNKIROOT/code/apps/Managed Software Center" > /dev/null
 /usr/bin/xcodebuild -project "Managed Software Center.xcodeproj" -alltargets clean > /dev/null
 /usr/bin/xcodebuild -project "Managed Software Center.xcodeproj" -alltargets build > /dev/null
@@ -376,8 +376,6 @@ cp -R "$MSCAPP" "$APPROOT/Applications/"
 cp -R "$MSAPP" "$APPROOT/Applications/Managed Software Center.app/Contents/Resources/"
 # make sure not writeable by group or other
 chmod -R go-w "$APPROOT/Applications/Managed Software Center.app"
-# make a symlink for the old MSU.app
-ln -s "../Managed Software Center.app" "$APPROOT/Applications/Utilities/Managed Software Update.app"
 # Create package info file.
 APPSIZE=`du -sk $APPROOT | cut -f1`
 NFILES=$(echo `find $APPROOT/ | wc -l`)
