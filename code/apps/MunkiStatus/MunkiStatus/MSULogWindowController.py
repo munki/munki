d9ef976d4c1116290e163fba628c39b28c354902
Greg Neagle <gregneagle@mac.com>
2016-04-21 11:12:08 -0700

    Better log window activation behavior when there is no loginwindow PolicyBanner on El Cap+

--- code/apps/MunkiStatus/MunkiStatus/MSULogWindowController.py
+++ code/apps/MunkiStatus/MunkiStatus/MSULogWindowController.py
@@ -11,25 +11,38 @@
 #
 
 from objc import YES, NO, IBAction, IBOutlet
+# pylint: disable=wildcard-import
+# pylint: disable=unused-wildcard-import
+# pylint: disable=redefined-builtin
 from Foundation import *
 from AppKit import *
+# pylint: enable=redefined-builtin
+# pylint: enable=wildcard-import
 
 import munki
 import os
+
+# lots of camelCase names, following Cocoa convention
+# pylint: disable=invalid-name
 
 
 class MSULogViewDataSource(NSObject):
     """Data source for an NSTableView that displays an array of text lines.
     Line breaks are assumed to be LF, and partial lines from incremental
     reading is handled."""
-    
+
+    # since this subclasses NSObject,
+    # it doesn't have a Python __init__method
+    # pylint: disable=no-init
+
     logFileData = NSMutableArray.alloc().init()
     filteredData = logFileData
-    
+
     lastLineIsPartial = False
-    filter = ''
-    
-    def tableView_writeRowsWithIndexes_toPasteboard_(self, aTableView, rowIndexes, pasteboard):
+    filterText = ''
+
+    def tableView_writeRowsWithIndexes_toPasteboard_(
+            self, aTableView, rowIndexes, pasteboard):
         '''Implements drag-n-drop of text rows to external apps'''
         text_to_copy = ''
         index_set = aTableView.selectedRowIndexes()
@@ -41,12 +54,14 @@ class MSULogViewDataSource(NSObject):
         #changeCount = pasteboard.clearContents()
         result = pasteboard.writeObjects_([text_to_copy])
         return YES
-    
+
     def applyFilterToData(self):
         '''Filter our log data'''
-        if len(self.filter):
-            filterPredicate = NSPredicate.predicateWithFormat_('self CONTAINS[cd] %@', self.filter)
-            self.filteredData = self.logFileData.filteredArrayUsingPredicate_(filterPredicate)
+        if len(self.filterText):
+            filterPredicate = NSPredicate.predicateWithFormat_(
+                'self CONTAINS[cd] %@', self.filterText)
+            self.filteredData = (
+                self.logFileData.filteredArrayUsingPredicate_(filterPredicate))
         else:
             self.filteredData = self.logFileData
 
@@ -60,19 +75,19 @@ class MSULogViewDataSource(NSObject):
             self.logFileData.addObject_(line)
         self.lastLineIsPartial = isPartial
         self.applyFilterToData()
-    
+
     def removeAllLines(self):
         '''Remove all data from our datasource'''
         self.logFileData.removeAllObjects()
-    
+
     def lineCount(self):
         '''Return the number of lines in our filtered data'''
         return self.filteredData.count()
-    
+
     def numberOfRowsInTableView_(self, tableView):
         '''Required datasource method'''
         return self.lineCount()
-    
+
     def tableView_objectValueForTableColumn_row_(self, tableView, column, row):
         '''Required datasource method -- returns the text data for the
         given row and column'''
@@ -85,16 +100,20 @@ class MSULogViewDataSource(NSObject):
 class MSULogWindowController(NSObject):
     '''Controller object for our log window'''
 
+    # since this subclasses NSObject,
+    # it doesn't have a Python __init__method
+    # pylint: disable=no-init
+
     window = IBOutlet()
     logView = IBOutlet()
     searchField = IBOutlet()
     pathControl = IBOutlet()
-    
+
     logFileData = MSULogViewDataSource.alloc().init()
-    
+
     fileHandle = None
     updateTimer = None
-    
+
     def copy_(self, sender):
         '''Implements copy operation so we can copy data from table view'''
         text_to_copy = ''
@@ -112,7 +131,7 @@ class MSULogWindowController(NSObject):
     def searchFilterChanged_(self, sender):
         '''User changed the search field'''
         filterString = self.searchField.stringValue().lower()
-        self.logFileData.filter = filterString
+        self.logFileData.filterText = filterString
         self.logFileData.applyFilterToData()
         self.logView.reloadData()
 
@@ -138,7 +157,7 @@ class MSULogWindowController(NSObject):
     @IBAction
     def showLogWindow_(self, notification):
         '''Show the log window.'''
-        
+
         consoleuser = munki.getconsoleuser()
         if consoleuser == None or consoleuser == u"loginwindow":
             self.window.setCanBecomeVisibleWithoutLogin_(True)
@@ -150,7 +169,7 @@ class MSULogWindowController(NSObject):
         windowRect.origin.y = 200.0
         windowRect.size.width -= 200.0
         windowRect.size.height -= 300.0
-        
+
         logfile = munki.pref('LogFile')
         self.pathControl.setURL_(NSURL.fileURLWithPath_(logfile))
         self.window.setTitle_(os.path.basename(logfile))
@@ -159,7 +178,8 @@ class MSULogWindowController(NSObject):
         self.watchLogFile_(logfile)
 
         # allow dragging from table view to outside of the app
-        self.logView.setDraggingSourceOperationMask_forLocal_(NSDragOperationAll, NO)
+        self.logView.setDraggingSourceOperationMask_forLocal_(
+            NSDragOperationAll, NO)
 
     def watchLogFile_(self, logFile):
         '''Display and continuously update a log file in the main window.'''
@@ -170,9 +190,11 @@ class MSULogWindowController(NSObject):
         self.fileHandle = NSFileHandle.fileHandleForReadingAtPath_(logFile)
         self.refreshLog()
         # Kick off a timer that updates the log view periodically.
-        self.updateTimer = NSTimer.scheduledTimerWithTimeInterval_target_selector_userInfo_repeats_(
-            0.25, self, u"refreshLog", None, YES)
-    
+        self.updateTimer = (
+            NSTimer.
+            scheduledTimerWithTimeInterval_target_selector_userInfo_repeats_(
+                0.25, self, self.refreshLog, None, YES))
+
     def stopWatching(self):
         '''Release the file handle and stop the update timer.'''
         if self.fileHandle is not None:
@@ -181,7 +203,7 @@ class MSULogWindowController(NSObject):
         if self.updateTimer is not None:
             self.updateTimer.invalidate()
             self.updateTimer = None
-    
+
     def refreshLog(self):
         '''Check for new available data, read it, and scroll to the bottom.'''
         data = self.fileHandle.availableData()
@@ -197,5 +219,6 @@ class MSULogWindowController(NSObject):
             self.logView.scrollRowToVisible_(self.logFileData.lineCount() - 1)
 
     def windowWillClose_(self, notification):
-        '''NSWindow delegate method -- if our window is closing, stop watching the log file.'''
+        '''NSWindow delegate method -- if our window is closing,
+        stop watching the log file.'''
         self.stopWatching()
