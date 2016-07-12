db9ccf02c65114d0c287ad005d31029138c7a5ad
Nate Walck <nate.walck@gmail.com>
2016-07-12 09:24:15 -0700

    Added ignore for line length as we don't enforce this currently

--- tests/travisci.py
+++ tests/travisci.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python env
 
 import subprocess
+import sys
 
 print("Checking code against pep8...")
 ps = subprocess.Popen(['git', 'diff', 'HEAD^'], stdout=subprocess.PIPE)
-subprocess.call(['flake8', '--diff'], stdin=ps.stdout)
+tests = subprocess.Popen(['flake8', '--diff', '--ignore=E501'], stdin=ps.stdout, stdout=subprocess.PIPE)
+out, err = tests.communicate()
+
+if out:
+    print(out)
+    print("Time to clean the lint...")
+    sys.exit(1)
+elif err:
+    print("An error occured!")
+    print(err)
+    sys.exit(1)
+else:
+    print("No lint errors, yay!")
+    sys.exit(0)
