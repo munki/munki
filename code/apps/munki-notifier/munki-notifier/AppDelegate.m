1aef2ca01946a4090f80c6c5c91f2a04be746da5
Greg Neagle <gregneagle@mac.com>
2017-11-16 17:37:30 -0800

    for UseNotificationCenterDays preference, munki-notifier will use a managed preference value if present, else fall back to reading the value from /Library/Preferences/ManagedInstalls.plist (ignoring user-level preferences)

--- code/apps/munki-notifier/munki-notifier/AppDelegate.m
+++ code/apps/munki-notifier/munki-notifier/AppDelegate.m
@@ -114,11 +114,20 @@ void launchMSCapp()
         if (plistRef && CFGetTypeID(plistRef) == CFNumberGetTypeID()) {
             oldestUpdateDays = [(NSNumber *)CFBridgingRelease(plistRef) floatValue];
         }
-        plistRef = CFPreferencesCopyValue(
-            CFSTR("UseNotificationCenterDays"), CFSTR("ManagedInstalls"), kCFPreferencesAnyUser, kCFPreferencesCurrentHost);
+        if (CFPreferencesAppValueIsForced(CFSTR("UseNotificationCenterDays"), CFSTR("ManagedInstalls"))) {
+            // use CFPreferencesCopyAppValue so we can respect managed preferences
+            plistRef = CFPreferencesCopyAppValue(
+                CFSTR("UseNotificationCenterDays"), CFSTR("ManagedInstalls"));
+        } else {
+            // preference not managed, read from /Library/Preferences (ignoring user-level preferences)
+            plistRef = CFPreferencesCopyValue(
+                CFSTR("UseNotificationCenterDays"), CFSTR("ManagedInstalls"),
+                kCFPreferencesAnyUser, kCFPreferencesCurrentHost);
+        }
         if (plistRef && CFGetTypeID(plistRef) == CFNumberGetTypeID()) {
             useNotificationCenterDays = [(NSNumber *)CFBridgingRelease(plistRef) integerValue];
         }
+        //NSLog(@"UseNotificationCenterDays: %ld", useNotificationCenterDays);
         plistRef = CFPreferencesCopyValue(
              CFSTR("ForcedUpdateDueDate"), CFSTR("ManagedInstalls"), kCFPreferencesAnyUser, kCFPreferencesCurrentHost);
         if (plistRef && CFGetTypeID(plistRef) == CFDateGetTypeID()) {
