c5da3d3072f121e0010c3a5fd6f769f3523ccbfe
cintom00 <tom.cinbis@unibas.ch>
2018-02-05 14:56:44 +0100

    WIP: Implemented optional_installs for localonly manifest
    You can now add an optional_installs key to you localonly manifest,
    which was not possible before. Remember to set the name of your
    localonly by using defaults command.

--- code/client/munkilib/updatecheck/core.py
+++ code/client/munkilib/updatecheck/core.py
@@ -174,6 +174,16 @@ def check(client_id='', localmanifestpath=None):
                     localonlymanifest, 'managed_uninstalls') or []
                 for item in localonlyuninstalls:
                     analyze.process_removal(item, cataloglist, installinfo)
+
+                localonlyoptionals = manifestutils.get_manifest_value_for_key(localonlymanifest,
+                                                                              'optional_installs') or []
+                for item in localonlyoptionals:
+                    dummy_result = analyze.process_optional_install(
+                        item,
+                        cataloglist,
+                        installinfo
+                    )
+
             else:
                 display.display_debug1(
                     "LocalOnlyManifest %s is set but is not present. "
