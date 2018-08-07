7f0c1a77cde156aeab1b71fdaf95e7646b30b750
Greg Neagle <gregneagle@mac.com>
2018-08-07 15:27:55 -0700

    Fix for category page views

--- code/apps/swift/Managed Software Center/Managed Software Center/Socket.swift
+++ code/apps/swift/Managed Software Center/Managed Software Center/Socket.swift
@@ -112,41 +112,40 @@ class UNIXDomainSocketClient {
         let intOffset = Int(fd / 32)
         let bitOffset = fd % 32
         let mask = Int32(1 << bitOffset)
-        set.fds_bits[0] = set.fds_bits[0] | mask
         switch intOffset {
-        case 0: set.fds_bits.0 = set.fds_bits.0 | mask
-        case 1: set.fds_bits.1 = set.fds_bits.1 | mask
-        case 2: set.fds_bits.2 = set.fds_bits.2 | mask
-        case 3: set.fds_bits.3 = set.fds_bits.3 | mask
-        case 4: set.fds_bits.4 = set.fds_bits.4 | mask
-        case 5: set.fds_bits.5 = set.fds_bits.5 | mask
-        case 6: set.fds_bits.6 = set.fds_bits.6 | mask
-        case 7: set.fds_bits.7 = set.fds_bits.7 | mask
-        case 8: set.fds_bits.8 = set.fds_bits.8 | mask
-        case 9: set.fds_bits.9 = set.fds_bits.9 | mask
-        case 10: set.fds_bits.10 = set.fds_bits.10 | mask
-        case 11: set.fds_bits.11 = set.fds_bits.11 | mask
-        case 12: set.fds_bits.12 = set.fds_bits.12 | mask
-        case 13: set.fds_bits.13 = set.fds_bits.13 | mask
-        case 14: set.fds_bits.14 = set.fds_bits.14 | mask
-        case 15: set.fds_bits.15 = set.fds_bits.15 | mask
-        case 16: set.fds_bits.16 = set.fds_bits.16 | mask
-        case 17: set.fds_bits.17 = set.fds_bits.17 | mask
-        case 18: set.fds_bits.18 = set.fds_bits.18 | mask
-        case 19: set.fds_bits.19 = set.fds_bits.19 | mask
-        case 20: set.fds_bits.20 = set.fds_bits.20 | mask
-        case 21: set.fds_bits.21 = set.fds_bits.21 | mask
-        case 22: set.fds_bits.22 = set.fds_bits.22 | mask
-        case 23: set.fds_bits.23 = set.fds_bits.23 | mask
-        case 24: set.fds_bits.24 = set.fds_bits.24 | mask
-        case 25: set.fds_bits.25 = set.fds_bits.25 | mask
-        case 26: set.fds_bits.26 = set.fds_bits.26 | mask
-        case 27: set.fds_bits.27 = set.fds_bits.27 | mask
-        case 28: set.fds_bits.28 = set.fds_bits.28 | mask
-        case 29: set.fds_bits.29 = set.fds_bits.29 | mask
-        case 30: set.fds_bits.30 = set.fds_bits.30 | mask
-        case 31: set.fds_bits.31 = set.fds_bits.31 | mask
-        default: break
+            case 0: set.fds_bits.0 = set.fds_bits.0 | mask
+            case 1: set.fds_bits.1 = set.fds_bits.1 | mask
+            case 2: set.fds_bits.2 = set.fds_bits.2 | mask
+            case 3: set.fds_bits.3 = set.fds_bits.3 | mask
+            case 4: set.fds_bits.4 = set.fds_bits.4 | mask
+            case 5: set.fds_bits.5 = set.fds_bits.5 | mask
+            case 6: set.fds_bits.6 = set.fds_bits.6 | mask
+            case 7: set.fds_bits.7 = set.fds_bits.7 | mask
+            case 8: set.fds_bits.8 = set.fds_bits.8 | mask
+            case 9: set.fds_bits.9 = set.fds_bits.9 | mask
+            case 10: set.fds_bits.10 = set.fds_bits.10 | mask
+            case 11: set.fds_bits.11 = set.fds_bits.11 | mask
+            case 12: set.fds_bits.12 = set.fds_bits.12 | mask
+            case 13: set.fds_bits.13 = set.fds_bits.13 | mask
+            case 14: set.fds_bits.14 = set.fds_bits.14 | mask
+            case 15: set.fds_bits.15 = set.fds_bits.15 | mask
+            case 16: set.fds_bits.16 = set.fds_bits.16 | mask
+            case 17: set.fds_bits.17 = set.fds_bits.17 | mask
+            case 18: set.fds_bits.18 = set.fds_bits.18 | mask
+            case 19: set.fds_bits.19 = set.fds_bits.19 | mask
+            case 20: set.fds_bits.20 = set.fds_bits.20 | mask
+            case 21: set.fds_bits.21 = set.fds_bits.21 | mask
+            case 22: set.fds_bits.22 = set.fds_bits.22 | mask
+            case 23: set.fds_bits.23 = set.fds_bits.23 | mask
+            case 24: set.fds_bits.24 = set.fds_bits.24 | mask
+            case 25: set.fds_bits.25 = set.fds_bits.25 | mask
+            case 26: set.fds_bits.26 = set.fds_bits.26 | mask
+            case 27: set.fds_bits.27 = set.fds_bits.27 | mask
+            case 28: set.fds_bits.28 = set.fds_bits.28 | mask
+            case 29: set.fds_bits.29 = set.fds_bits.29 | mask
+            case 30: set.fds_bits.30 = set.fds_bits.30 | mask
+            case 31: set.fds_bits.31 = set.fds_bits.31 | mask
+            default: break
         }
     }
     
