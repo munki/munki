5d3b7b0636fb04ec8dac73ad0622868ba1deb73b
Greg Neagle <gregneagle@mac.com>
2019-04-04 06:35:03 -0700

    Highlight toolbar item imeediately after click instead of waiting for page load.

--- code/apps/Managed Software Center/Managed Software Center/Controllers/MainWindowController.swift
+++ code/apps/Managed Software Center/Managed Software Center/Controllers/MainWindowController.swift
@@ -1340,21 +1340,25 @@ class MainWindowController: NSWindowController, NSWindowDelegate, WKNavigationDe
     
     @IBAction func softwareToolbarItemClicked(_ sender: Any) {
         // User clicked Software toolbar button
+        highlightToolbarButtons("Software")
         loadAllSoftwarePage(sender)
     }
     
     @IBAction func categoriesToolbarItemClicked(_ sender: Any) {
         // User clicked Categories toolbar button'''
+        highlightToolbarButtons("Categories")
         loadCategoriesPage(sender)
     }
     
     @IBAction func myItemsToolbarItemClicked(_ sender: Any) {
         // User clicked My Items toolbar button'''
+        highlightToolbarButtons("My Items")
         loadMyItemsPage(sender)
     }
     
     @IBAction func updatesToolbarItemClicked(_ sender: Any) {
         // User clicked Updates toolbar button'''
+        highlightToolbarButtons("Updates")
         loadUpdatesPage(sender)
     }
     
