72a0535d0d0b2e940e9fa158f759fe01e59cbff3
Greg Neagle <gregneagle@mac.com>
2020-01-01 08:53:37 -0800

    Update Copyright dates

--- code/apps/Managed Software Center/Managed Software Center/MunkiItems.swift
+++ code/apps/Managed Software Center/Managed Software Center/MunkiItems.swift
@@ -3,7 +3,7 @@
 //  Managed Software Center
 //
 //  Created by Greg Neagle on 6/7/18.
-//  Copyright © 2018-2019 The Munki Project. All rights reserved.
+//  Copyright © 2018-2020 The Munki Project. All rights reserved.
 //
 
 import Foundation
