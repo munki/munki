b58010d0c84421864b098a9d122ccf9398a92020
Steve Küng <steve.kueng@swisstxt.ch>
2020-03-11 15:10:31 +0100

    fix munki status

--- code/apps/MunkiStatus/MunkiStatus/MSWindow.swift
+++ code/apps/MunkiStatus/MunkiStatus/MSWindow.swift
@@ -2,8 +2,8 @@
 //  MSWindow.swift
 //  MunkiStatus
 //
-//  Created by Steve Küng on 16.10.18.
-//  Copyright © 2018 The Munki Project. All rights reserved.
+//  Created by Steve Küng on 11.03.20.
+//  Copyright © 2020 The Munki Project. All rights reserved.
 //
 
 import Cocoa
