e87d74b77c514e034485578fe32400e11f41bca2
Greg Neagle <gregneagle@mac.com>
2020-09-10 18:50:42 -0700

    Make the title bar area of the main (web) view draggable. Addresses #1017

--- code/apps/Managed Software Center/Managed Software Center/MSCWebView.swift
+++ code/apps/Managed Software Center/Managed Software Center/MSCWebView.swift
@@ -27,5 +27,45 @@ class MSCWebView: WKWebView {
             }
         }
     }
+    
+    override func mouseDown(with mouseDownEvent: NSEvent) {
+        let window = self.window!
+        let startingPoint = mouseDownEvent.locationInWindow
+        guard let menubarheight = NSApp.mainMenu?.menuBarHeight else {
+            return super.mouseDown(with: mouseDownEvent)
+        }
+        if startingPoint.y < window.frame.height - menubarheight {
+            return super.mouseDown(with: mouseDownEvent)
+        }
+        
+        // Track events until the mouse is up (in which we interpret as a click), or a drag starts (in which we pass off to the Window Server to perform the drag)
+        var shouldCallSuper = false
+        
+        // trackEvents won't return until after the tracking all ends
+        window.trackEvents(matching: [.leftMouseDragged, .leftMouseUp], timeout:NSEvent.foreverDuration, mode: RunLoop.Mode.default) { event, stop in
+            switch event?.type {
+                case .leftMouseUp:
+                    // Stop on a mouse up; post it back into the queue and call super so it can handle it
+                    shouldCallSuper = true
+                    NSApp.postEvent(event!, atStart: false)
+                    stop.pointee = true
+                
+                case .leftMouseDragged:
+                    // track mouse drags, and if more than a few points are moved we start a drag
+                    let currentPoint = event!.locationInWindow
+                    if (abs(currentPoint.x - startingPoint.x) >= 5 || abs(currentPoint.y - startingPoint.y) >= 5) {
+                        stop.pointee = true
+                        window.performDrag(with: event!)
+                    }
+                
+                default:
+                    break
+            }
+        }
+                
+        if (shouldCallSuper) {
+            super.mouseDown(with: mouseDownEvent)
+        }
+    }
 
 }
