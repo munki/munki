bb638868a3f787cc9a96b957d8f28dae2d26a0cc
Greg Neagle <gregneagle@mac.com>
2021-09-24 19:48:01 -0700

    Update CatalogURL for Apple Softwar Update in Big Sur

--- code/client/munkilib/appleupdates/sync.py
+++ code/client/munkilib/appleupdates/sync.py
@@ -95,7 +95,7 @@ DEFAULT_CATALOG_URLS = {
               'index-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-'
               'mountainlion-lion-snowleopard-leopard.merged-1.sucatalog'),
     '11': ('https://swscan.apple.com/content/catalogs/others/'
-           'index-11-10.15-10.14-10.13-10.12-10.11-10.10-10.9-'
+           'index-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-'
            'mountainlion-lion-snowleopard-leopard.merged-1.sucatalog'),
 }
 
