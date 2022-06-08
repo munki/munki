fa11dfb135d51d705fea180f154c1c091183a945
Greg Neagle <gregneagle@mac.com>
2022-06-08 11:07:38 -0700

    Add macOS 13 sucatalog URL

--- code/client/munkilib/appleupdates/sync.py
+++ code/client/munkilib/appleupdates/sync.py
@@ -100,7 +100,9 @@ DEFAULT_CATALOG_URLS = {
     '12': ('https://swscan.apple.com/content/catalogs/others/'
            'index-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-'
            'mountainlion-lion-snowleopard-leopard.merged-1.sucatalog'),
-    
+    '13': ('https://swscan.apple.com/content/catalogs/others/'
+           'index-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-'
+           'mountainlion-lion-snowleopard-leopard.merged-1.sucatalog'),
 }
 
 # Preference domain for Apple Software Update.
