8ef678fa9205a337955b292b0042bc516635354c
Greg Neagle <gregneagle@mac.com>
2024-07-06 17:59:15 -0700

    SwiftFormat formatting

--- code/cli/munki/makecatalogs/makecatalogs.swift
+++ code/cli/munki/makecatalogs/makecatalogs.swift
@@ -15,29 +15,29 @@ struct MakeCatalogs: ParsableCommand {
         abstract: "Builds Munki catalogs from pkginfo files.",
         usage: "makecatalogs [options] [<repo_path>]"
     )
-    
+
     @Flag(name: [.long, .customShort("V")], help: "Print the version of the munki tools and exit.")
     var version = false
-    
+
     @Flag(name: .shortAndLong, help: "Disable sanity checks.")
     var force = false
-    
+
     @Flag(name: .shortAndLong,
           help: "Skip checking of pkg existence. Useful when pkgs aren't on the same server as pkginfo, catalogs and manifests.")
     var skipPkgCheck = false
-    
+
     @Option(name: [.customLong("repo-url"), .customLong("repo_url")],
             help: "Optional repo URL that takes precedence over the default repo_url specified via preferences.")
     var repoURL = ""
-    
+
     @Option(help: "Specify a custom plugin to connect to the Munki repo.")
     var plugin = "FileRepo"
-    
+
     @Argument(help: "Path to Munki repo")
     var repo_path = ""
-    
+
     var actual_repo_url = ""
-    
+
     mutating func validate() throws {
         if version {
             // asking for version info; we don't need to validate there's a repo URL
@@ -47,7 +47,7 @@ struct MakeCatalogs: ParsableCommand {
         //   - as a file path provided at the command line
         //   - as a --repo_url option
         //   - as a preference stored in the com.googlecode.munki.munkiimport domain
-        if !repo_path.isEmpty && !repoURL.isEmpty {
+        if !repo_path.isEmpty, !repoURL.isEmpty {
             // user has specified _both_ repo_path and repo_url!
             throw ValidationError("Please specify only one of --repo_url or <repo_path>!")
         }
@@ -61,7 +61,7 @@ struct MakeCatalogs: ParsableCommand {
         } else if let pref_repo_url = adminPref("repo_url") as? String {
             actual_repo_url = pref_repo_url
         }
-        
+
         if actual_repo_url.isEmpty {
             throw ValidationError("Please specify --repo_url or a repo path.")
         }
@@ -72,13 +72,13 @@ struct MakeCatalogs: ParsableCommand {
             print(getVersion())
             return
         }
-        
+
         let options = MakeCatalogOptions(
             skipPkgCheck: skipPkgCheck,
             force: force,
             verbose: true
         )
-        
+
         do {
             let repo = try repoConnect(url: actual_repo_url, plugin: plugin)
             // TODO: implement repo defining its own makecatalogs method
