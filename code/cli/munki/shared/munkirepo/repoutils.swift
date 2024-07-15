4820386197e30644a064f9ae477169978356e087
Greg Neagle <gregneagle@mac.com>
2024-07-15 10:15:23 -0700

    Move listItemsOfKind function

--- code/cli/munki/shared/munkirepo/repoutils.swift
+++ code/cli/munki/shared/munkirepo/repoutils.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+// TODO: get rid of this
+func listItemsOfKind(_ repo: Repo, _ kind: String) throws -> [String] {
+    // Returns a list of items of kind. Relative pathnames are prepended
+    // with kind. (example: ["icons/Bar.png", "icons/Foo.png"])
+    // Could throw RepoError
+    let itemlist = try repo.list(kind)
+    return itemlist.map(
+        { (kind as NSString).appendingPathComponent($0) }
+    )
+}
