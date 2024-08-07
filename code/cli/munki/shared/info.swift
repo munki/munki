c46be1d07eb68c25af20b2dc82396ee7782c61aa
Greg Neagle <gregneagle@mac.com>
2024-08-07 16:20:24 -0700

    Implement more info functions

--- code/cli/munki/shared/info.swift
+++ code/cli/munki/shared/info.swift
@@ -5,6 +5,7 @@
 //  Created by Greg Neagle on 8/7/24.
 //
 
+import CoreServices.LaunchServices
 import Foundation
 
 func platform() -> String {
@@ -24,4 +25,59 @@ func platform() -> String {
 func isAppleSilicon() -> Bool {
     // Returns true if we're running on Apple silicon"
     return platform() == "arm64"
+}
+
+// private, undocumented LaunchServices function
+@_silgen_name("_LSCopyAllApplicationURLs") func LSCopyAllApplicationURLs(_: UnsafeMutablePointer<NSMutableArray?>) -> OSStatus
+func launchServicesInstalledApps() -> [String]? {
+    var apps: NSMutableArray?
+    if LSCopyAllApplicationURLs(&apps) == 0 {
+        if let appURLs = (apps! as NSArray) as? [URL] {
+            let pathsArray = appURLs.map(\.path)
+            return pathsArray
+        }
+    }
+    return nil
+}
+
+func spApplicationData() async -> PlistDict {
+    // Uses system profiler to get application info for this machine
+    // Returns a dictionary with app paths as keys
+    var applicationData = PlistDict()
+    let tool = "/usr/sbin/system_profiler"
+    let arguments = ["SPApplicationsDataType", "-xml"]
+    do {
+        let result = try await runCliAsync(tool, arguments: arguments, timeout: 120)
+        if result.exitcode != 0 {
+            throw ProcessError.error(description: result.error)
+        }
+        if let plist = try readPlist(fromString: result.output) as? [PlistDict] {
+            // system_profiler xml is an array
+            if let items = plist[0]["_items"] as? [PlistDict] {
+                for item in items {
+                    if let path = item["path"] as? String {
+                        applicationData[path] = item
+                    }
+                }
+            } else {
+                throw PlistError.readError(description: "output is wrong format")
+            }
+        } else {
+            throw PlistError.readError(description: "output is wrong format")
+        }
+    } catch let PlistError.readError(description) {
+        displayError("Could not parse output from system_profiler; skipping SPApplicationsDataType query: \(description)")
+        return applicationData
+    } catch ProcessError.timeout {
+        displayError("system_profiler hung; skipping SPApplicationsDataType query")
+        return applicationData
+    } catch let ProcessError.error(description) {
+        displayError("Unexpected error with system_profiler; skipping SPApplicationsDataType query: \(description)")
+        return applicationData
+    } catch {
+        displayError("Unexpected error with system_profiler; skipping SPApplicationsDataType query")
+        return applicationData
+    }
+
+    return applicationData
 }
