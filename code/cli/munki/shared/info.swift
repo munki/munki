8aa340da4c6f91714d84325331376bbe8170da61
Greg Neagle <gregneagle@mac.com>
2024-08-07 10:47:30 -0700

    Starting on info functions

--- code/cli/munki/shared/info.swift
+++ code/cli/munki/shared/info.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+func platform() -> String {
+    // returns platform ("x86_64", "arm64")
+    var systemInfo = utsname()
+    uname(&systemInfo)
+    let size = Int(_SYS_NAMELEN) // is 32, but posix AND its init is 256....
+
+    let s = withUnsafeMutablePointer(to: &systemInfo.machine) { p in
+        p.withMemoryRebound(to: CChar.self, capacity: size) { p2 in
+            return String(cString: p2)
+        }
+    }
+    return s
+}
+
+func isAppleSilicon() -> Bool {
+    // Returns true if we're running on Apple silicon"
+    return platform() == "arm64"
+}
