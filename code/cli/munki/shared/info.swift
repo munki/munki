874145a3cb6f23985f47af98ec9fb4f0624308b4
Greg Neagle <gregneagle@mac.com>
2024-08-08 09:19:14 -0700

    Implement more info functions

--- code/cli/munki/shared/info.swift
+++ code/cli/munki/shared/info.swift
@@ -7,6 +7,7 @@
 
 import CoreServices.LaunchServices
 import Foundation
+import IOKit
 
 func platform() -> String {
     // returns platform ("x86_64", "arm64")
@@ -66,18 +67,149 @@ func spApplicationData() async -> PlistDict {
             throw PlistError.readError(description: "output is wrong format")
         }
     } catch let PlistError.readError(description) {
-        displayError("Could not parse output from system_profiler; skipping SPApplicationsDataType query: \(description)")
+        displayWarning("Could not parse output from system_profiler; skipping SPApplicationsDataType query: \(description)")
         return applicationData
     } catch ProcessError.timeout {
-        displayError("system_profiler hung; skipping SPApplicationsDataType query")
+        displayWarning("system_profiler hung; skipping SPApplicationsDataType query")
         return applicationData
     } catch let ProcessError.error(description) {
-        displayError("Unexpected error with system_profiler; skipping SPApplicationsDataType query: \(description)")
+        displayWarning("Unexpected error with system_profiler; skipping SPApplicationsDataType query: \(description)")
         return applicationData
     } catch {
-        displayError("Unexpected error with system_profiler; skipping SPApplicationsDataType query")
+        displayWarning("Unexpected error with system_profiler; skipping SPApplicationsDataType query")
         return applicationData
     }
 
     return applicationData
+}
+
+func getSystemProfilerData(_ dataType: String) async -> PlistDict {
+    // Uses system profiler to get info of data_type for this machine
+    let tool = "/usr/sbin/system_profiler"
+    let arguments = [dataType, "-xml"]
+    let result = await runCliAsync(tool, arguments: arguments)
+    if result.exitcode != 0 {
+        return PlistDict()
+    }
+    do {
+        if let plist = try readPlist(fromString: result.output) as? [PlistDict] {
+            // system_profiler xml is an array
+            if let items = plist[0]["_items"] as? [PlistDict],
+               items.count > 0
+            {
+                return items[0]
+            }
+        }
+    } catch {
+        return PlistDict()
+    }
+    return PlistDict()
+}
+
+func getHardwareInfo() async -> PlistDict {
+    // Uses system profiler to get hardware info for this machine
+    return await getSystemProfilerData("SPHardwareDataType")
+}
+
+func getIBridgeInfo() async -> PlistDict {
+    // Uses system profiler to get iBridge info for this machine
+    return await getSystemProfilerData("SPiBridgeDataType")
+}
+
+func getIPAddresses(_ kind: String) async -> [String] {
+    // Uses system profiler to get active IP addresses for this machine
+    // kind must be one of 'IPv4' or 'IPv6'
+    // NOTE this does not return any utun addresses.
+    var ipAddresses = [String]()
+    let tool = "/usr/sbin/system_profiler"
+    let arguments = ["SPNetworkDataType", "-xml"]
+    let result = await runCliAsync(tool, arguments: arguments)
+    if result.exitcode != 0 {
+        return ipAddresses
+    }
+    do {
+        if let plist = try readPlist(fromString: result.output) as? [PlistDict] {
+            // system_profiler xml is an array
+            if let items = plist[0]["_items"] as? [PlistDict] {
+                for item in items {
+                    if let addressType = item[kind] as? PlistDict,
+                       let addresses = addressType["Addresses"] as? [String]
+                    {
+                        ipAddresses += addresses
+                    }
+                }
+            }
+        }
+    } catch {
+        return ipAddresses
+    }
+    return ipAddresses
+}
+
+// IOKit helpers
+
+private func serviceMatching(_ className: String) -> io_registry_entry_t {
+    // returns a reference to an IOKit service matching on IOService class name,
+    // typically something like "IOPlatformExpertDevice"
+    return IOServiceGetMatchingService(
+        kIOMasterPortDefault, IOServiceMatching(className)
+    )
+}
+
+private func serviceNameMatching(_ name: String) -> io_registry_entry_t {
+    // returns a reference to an IOKit service matching on IOService name
+    return IOServiceGetMatchingService(
+        kIOMasterPortDefault, IOServiceNameMatching(name)
+    )
+}
+
+private func stringValueForIOServiceProperty(service: io_registry_entry_t, key: String) -> String? {
+    // attempts to return a string value for the given property key
+    let rawData = IORegistryEntryCreateCFProperty(
+        service, key as CFString, kCFAllocatorDefault, 0
+    )
+    if rawData == nil {
+        return nil
+    }
+    let data = rawData?.takeRetainedValue() as! CFData
+    return String(data: data as Data,
+                  encoding: .utf8)?.trimmingCharacters(in: ["\0"])
+}
+
+func serialNumber() -> String {
+    // Returns the serial number of this Mac
+    let serial = IORegistryEntryCreateCFProperty(
+        serviceMatching("IOPlatformExpertDevice"),
+        kIOPlatformSerialNumberKey as CFString,
+        kCFAllocatorDefault,
+        0
+    )
+    if let serial = serial?.takeRetainedValue() {
+        return (serial as! CFString) as String
+    }
+    return ""
+}
+
+func productName() -> String {
+    // Returns the product name from IORegistry
+    return stringValueForIOServiceProperty(
+        service: serviceNameMatching("product"),
+        key: "product-name"
+    ) ?? ""
+}
+
+func boardID() -> String {
+    // Returns board-id from IORegistry
+    return stringValueForIOServiceProperty(
+        service: serviceMatching("IOPlatformExpertDevice"),
+        key: "board-id"
+    ) ?? "<none>"
+}
+
+func deviceID() -> String {
+    // Returns board-id from IORegistry
+    return stringValueForIOServiceProperty(
+        service: serviceMatching("IOPlatformExpertDevice"),
+        key: "target-sub-type"
+    ) ?? "<none>"
 }
