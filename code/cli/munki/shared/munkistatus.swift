081cb126ec97e541103a7f71581be515b59109d4
Greg Neagle <gregneagle@mac.com>
2024-09-11 16:46:37 -0700

    Convert more comments to documentation

--- code/cli/munki/shared/munkistatus.swift
+++ code/cli/munki/shared/munkistatus.swift
@@ -20,8 +20,8 @@
 
 import Foundation
 
+/// a Singleton class that contains our current status for display
 class MunkiStatusInfo {
-    // a Singleton class
     static let shared = MunkiStatusInfo()
 
     var status: PlistDict
@@ -39,16 +39,14 @@ class MunkiStatusInfo {
     }
 }
 
+/// Uses launchd KeepAlive path so it launches from a launchd agent
+/// in the correct context.
+/// This is more complicated to set up, but makes Apple (and launchservices) happier.
+/// There needs to be a launch agent that is triggered when the launchfile is created;
+/// and that launch agent then runs MunkiStatus.app.
+/// Note: this only works at the loginwindow, since the LaunchAgent is only
+/// loaded at the loginwindow.
 func munkiStatusLaunch() {
-    // Uses launchd KeepAlive path so it launches from a launchd agent
-    // in the correct context.
-    // This is more complicated to set up, but makes Apple (and launchservices)
-    // happier.
-    // There needs to be a launch agent that is triggered when the launchfile
-    // is created; and that launch agent then runs MunkiStatus.app.
-    // Note: this only works at the loginwindow, since the LaucnhAgent is only
-    // loaded at the loginwindow.
-
     let launchfile = "/var/run/com.googlecode.munki.MunkiStatus"
     if FileManager.default.createFile(atPath: launchfile, contents: nil) {
         usleep(1_000_000)
@@ -58,8 +56,8 @@ func munkiStatusLaunch() {
     }
 }
 
+/// Post a status notification
 func postStatusNotification() {
-    // Post a status notification
     let NOTIFICATION_NAME = "com.googlecode.munki.managedsoftwareupdate.statusUpdate"
     let dnc = DistributedNotificationCenter.default()
     dnc.postNotificationName(
@@ -70,68 +68,68 @@ func postStatusNotification() {
     )
 }
 
+/// Sets the status message.
 func munkiStatusMessage(_ text: String) {
-    // Sets the status message.
     MunkiStatusInfo.shared.status["message"] = text
     postStatusNotification()
 }
 
+/// Sets the detail text.
 func munkiStatusDetail(_ text: String) {
-    // Sets the detail text.
     MunkiStatusInfo.shared.status["detail"] = text
     postStatusNotification()
 }
 
 func munkiStatusPercent(_ percentage: Int) {
-    // Sets the progress indicator to 0-100 percent done.
-    // If you pass a negative number, the progress indicator
-    // is shown as an indeterminate indicator.
     MunkiStatusInfo.shared.status["percent"] = percentage
     postStatusNotification()
 }
 
+/// Sets the progress indicator to 0-100 percent done.
+/// If you pass a negative number, the progress indicator
+/// is shown as an indeterminate indicator.
 func munkiStatusHideStopButton() {
     // Tells MunkiStatus/MSC to hide the stop button.
     MunkiStatusInfo.shared.status["stop_button_visible"] = false
     postStatusNotification()
 }
 
+/// Tells MunkiStatus/MSC to show the stop button.
 func munkiStatusShowStopButton() {
-    // Tells MunkiStatus/MSC to show the stop button.
     MunkiStatusInfo.shared.status["stop_button_visible"] = true
     postStatusNotification()
 }
 
+/// Tells MunkiStatus/MSC to disable the stop button.
 func munkiStatusDisableStopButton() {
-    // Tells MunkiStatus/MSC to disable the stop button.
     MunkiStatusInfo.shared.status["stop_button_enabled"] = false
     postStatusNotification()
 }
 
+/// Tells MunkiStatus/MSC to enable the stop button.
 func munkiStatusEnableStopButton() {
-    // Tells MunkiStatus/MSC to enable the stop button.
     MunkiStatusInfo.shared.status["stop_button_enabled"] = true
     postStatusNotification()
 }
 
+/// Tells MunkiStatus to bring its window to the front
 func munkiStatusActivate() {
-    // Tells MunkiStatus to bring its window to the front
     MunkiStatusInfo.shared.status["command"] = "activate"
     postStatusNotification()
     // commands should be cleared after they are sent
     MunkiStatusInfo.shared.status["command"] = ""
 }
 
+/// Tells MunkiStatus to quit
 func munkiStatusQuit() {
-    // Tells MunkiStatus to quit
     MunkiStatusInfo.shared.status["command"] = "quit"
     postStatusNotification()
     // commands should be cleared after they are sent
     MunkiStatusInfo.shared.status["command"] = ""
 }
 
+/// Tells MunkiStatus to display a restart alert
 func munkiStatusRestartAlert() {
-    // Tells MunkiStatus to display a restart alert
     MunkiStatusInfo.shared.status["command"] = "showRestartAlert"
     postStatusNotification()
     // commands should be cleared after they are sent
