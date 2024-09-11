544138152bc8a8f70ded9f09fefc7d5e4b73f28b
Greg Neagle <gregneagle@mac.com>
2024-09-10 20:34:19 -0700

    Finally getting the hang of how documentation comments work

--- code/cli/munki/shared/network/urls.swift
+++ code/cli/munki/shared/network/urls.swift
@@ -7,19 +7,24 @@
 
 import Foundation
 
+/// Uses URL to compose a URL from basURL and additional relative path
 func composedURLWithBase(_ baseURLString: String, adding path: String) -> String {
     let baseURL = URL(string: baseURLString)
     let composedURL = URL(string: path, relativeTo: baseURL)
     return composedURL?.absoluteString ?? ""
 }
 
+/// Returns a URL to something in a Munki repo.
+/// If type is empty, returns the base URL for the Munki repo.
+/// If type is one of the supported types, returns the type-specific URL.
+/// If type is specifiied and resource is also specifiied, a full URL to the resource is
+/// constructed and returned
 func munkiRepoURL(_ type: String = "", resource: String = "") -> String? {
-    /// we could use composedURLWithBase, but that doesn't handle
-    /// URLs in the format of CGI invocations correctly, and would not
-    /// be consistent with the behavior of the Python version of Munki
-    /// So instead we'll do simple string concatenation
-    /// (with percent-encoding of the resource path)
-
+    // we could use composedURLWithBase, but that doesn't handle
+    // URLs in the format of CGI invocations correctly, and would not
+    // be consistent with the behavior of the Python version of Munki
+    // So instead we'll do simple string concatenation
+    // (with percent-encoding of the resource path)
     let munkiBaseURL = pref("SoftwareRepoURL") as? String ?? ""
     if type.isEmpty {
         return munkiBaseURL
