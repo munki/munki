003cd736537413c2fc0d38dd4b62d88385d907e9
Greg Neagle <gregneagle@mac.com>
2024-09-12 09:02:03 -0700

    Reset munkitester to basic sample

--- code/cli/munki/munkitester/main.swift
+++ code/cli/munki/munkitester/main.swift
@@ -9,30 +9,4 @@
 
 import Foundation
 
-DisplayOptions.shared.verbose = 3
-let options = GurlOptions(
-    url: "https://github.com/macadmins/munki-builds/releases/download/v6.5.1.4661/munkitools-6.5.1.4661.pkg",
-    destinationPath: "/tmp/munkitools-6.5.1.4661.pkg",
-    followRedirects: "all",
-    canResume: true,
-    downloadOnlyIfChanged: true
-)
-
-let connection = Gurl(options: options)
-connection.start()
-while true {
-    let done = connection.isDone()
-    if String(connection.status).hasPrefix("2"),
-       connection.percentComplete != -1 {
-        displayPercentDone(current: connection.percentComplete, maximum: 100)
-    } else if connection.bytesReceived > 0 {
-        displayDetail("Bytes received: \(connection.bytesReceived)")
-    }
-    if done {
-        break
-    }
-}
-
-if let error = connection.error {
-    print(error)
-}
+print(getVersion())
