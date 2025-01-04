d100df92305620d54fc76f59b2b11c3fe834642c
Greg Neagle <gregneagle@mac.com>
2025-01-04 08:10:23 -0800

    build_swift_munki.sh now builds with the 'Release' configuration

--- code/tools/build_swift_munki.sh
+++ code/tools/build_swift_munki.sh
@@ -10,6 +10,6 @@ check_exit_code() {
 SWIFT_MUNKI_DIR="./cli/munki"
 
 for TOOL in managedsoftwareupdate makecatalogs makepkginfo munkiimport removepackages app_usage_monitor appusaged launchapp iconimporter repoclean ; do
-    xcodebuild -project "$SWIFT_MUNKI_DIR/munki.xcodeproj" -scheme $TOOL build
+    xcodebuild -project "$SWIFT_MUNKI_DIR/munki.xcodeproj" -configuration Release -scheme $TOOL build
     check_exit_code "$?" "Error building $TOOL"
 done
