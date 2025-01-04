f6f15219cf1fd8f1410874c061d451191921e494
Greg Neagle <gregneagle@mac.com>
2025-01-04 08:01:41 -0800

    Modify build_swift_munki to build all tools in a loop

---
+++ code/tools/build_swift_munki.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+check_exit_code() {
+    if [ "$1" != "0" ]; then
+        echo "$2: $1" 1>&2
+        exit 1
+    fi
+}
+
+SWIFT_MUNKI_DIR="./cli/munki"
+
+for TOOL in managedsoftwareupdate makecatalogs makepkginfo munkiimport removepackages app_usage_monitor appusaged launchapp iconimporter repoclean ; do
+    xcodebuild -project "$SWIFT_MUNKI_DIR/munki.xcodeproj" -scheme $TOOL build
+    check_exit_code "$?" "Error building $TOOL"
+done
