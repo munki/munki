63e6442a7799b6b6dadaf90210d4e02c1c5e69d2
Greg Neagle <gregneagle@mac.com>
2025-04-13 20:52:54 -0700

    Implement more of manifestutil

---
+++ code/cli/munki/manifestutil/MUlistManifests.swift
@@ -0,0 +1,59 @@
+//
+//  MUlistManifests.swift
+//  manifestutil
+//
+//  Created by Greg Neagle on 4/13/25.
+//
+//  Copyright 2024-2025 Greg Neagle.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       https://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+import ArgumentParser
+import Foundation
+
+func getManifestNames(repo: Repo) throws -> [String] {
+    do {
+        let manifestNames = try repo.list("manifests")
+        return manifestNames.sorted()
+    } catch let error {
+        printStderr("Could not retrieve manifests: \(error.localizedDescription)")
+        throw ExitCode(-1)
+    }
+}
+
+extension ManifestUtil {
+    struct ListManifests: ParsableCommand {
+        static var configuration = CommandConfiguration(
+            abstract: "Lists available manifest in Munki repo.")
+        
+        @Argument(help: ArgumentHelp(
+            "String to match manifest names similar to file name globbing. To avoid the shell expanding wildcards, wrap the string in quotes.",
+            valueName: "match-string"
+        ))
+        var globString: String = ""
+        
+        func run() throws {
+            let repo = try connectToRepo()
+            let manifestNames = try getManifestNames(repo: repo)
+            if globString.isEmpty {
+                print(manifestNames.joined(separator: "\n"))
+            } else {
+                for name in manifestNames {
+                    if fnmatch(globString, name, 0) == 0 {
+                        print(name)
+                    }
+                }
+            }
+        }
+    }
+}
