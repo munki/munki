e15768882bebe09c9934373f59f0bcb702d19cc2
Greg Neagle <gregneagle@mac.com>
2025-05-09 13:34:48 -0700

    Add tests for new makepkginfolin function

---
+++ code/cli/munki/munkiCLItesting/makepkginfolibTests.swift
@@ -0,0 +1,51 @@
+//
+//  makepkginfolibTests.swift
+//  munkiCLItesting
+//
+//  Created by Greg Neagle on 5/9/25.
+//
+
+import Testing
+
+struct pkginfolibTests {
+    @Test func getOneMinimumOSVersionFromInstalls() {
+        let pkginfo: PlistDict = [
+            "installs": [
+                ["minosversion": "10.11.0"],
+            ],
+        ]
+        #expect(getMinimumOSVersionFromInstallsApps(pkginfo) == "10.11.0")
+    }
+
+    @Test func getHighestMinimumOSVersionFromInstalls() {
+        let pkginfo: PlistDict = [
+            "installs": [
+                ["minosversion": "11.0"],
+                ["minosversion": "10.15.0"],
+            ],
+        ]
+        #expect(getMinimumOSVersionFromInstallsApps(pkginfo) == "11.0")
+    }
+
+    @Test func getHighestMinimumOSVersionFromInstallsAndPkginfoHigher() {
+        let pkginfo: PlistDict = [
+            "installs": [
+                ["minosversion": "11.0"],
+                ["minosversion": "10.15.0"],
+            ],
+            "minimum_os_version": "12.0",
+        ]
+        #expect(getMinimumOSVersionFromInstallsApps(pkginfo) == "12.0")
+    }
+
+    @Test func getHighestMinimumOSVersionFromInstallsAndPkginfoLower() {
+        let pkginfo: PlistDict = [
+            "installs": [
+                ["minosversion": "11.0"],
+                ["minosversion": "10.15.0"],
+            ],
+            "minimum_os_version": "10.7",
+        ]
+        #expect(getMinimumOSVersionFromInstallsApps(pkginfo) == "11.0")
+    }
+}
