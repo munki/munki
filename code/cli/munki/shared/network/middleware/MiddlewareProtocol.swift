8a6902550d2ca9992281e6eca0181825ca197eef
Greg Neagle <gregneagle@mac.com>
2025-05-20 13:41:01 -0700

    Change pkginfo in middleware request to a 'let' declaration since it should not be changed by the middleware

--- code/cli/munki/shared/network/middleware/MiddlewareProtocol.swift
+++ code/cli/munki/shared/network/middleware/MiddlewareProtocol.swift
@@ -23,7 +23,7 @@ import Foundation
 public struct MunkiMiddlewareRequest {
     var url: String
     var headers: [String: String]
-    var pkginfo: PlistDict?
+    let pkginfo: PlistDict?
 }
 
 public protocol MunkiMiddleware {
