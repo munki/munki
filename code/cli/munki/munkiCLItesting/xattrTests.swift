76b06ceb17a5cda3917d8132ed376a13872d115e
Greg Neagle <gregneagle@mac.com>
2025-12-23 13:27:12 -0800

    Remove some odd text from opening comment block

--- code/cli/munki/munkiCLItesting/xattrTests.swift
+++ code/cli/munki/munkiCLItesting/xattrTests.swift
@@ -3,7 +3,7 @@
 //  munkiCLItesting
 //
 //  Created by Greg Neagle on 5/4/25.
-// istXattrs
+//
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
 //  You may obtain a copy of the License at
