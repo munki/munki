041c6a7efb5ecfcdd5f3274402ab3b23ab24c9f6
Greg Neagle <gregneagle@mac.com>
2026-02-02 16:50:06 -0800

    Revert "Borrow some code from (Swift) munki so we can get running processes without shelling out to 'ps' or 'pgrep'"
    This reverts commit ee645ef3d3347e94f05e5f9329032668a5f5935e.

--- code/apps/MunkiStatus/MunkiStatus/Utils.swift
+++ code/apps/MunkiStatus/MunkiStatus/Utils.swift
@@ -58,6 +58,60 @@ func isAppleSilicon() -> Bool {
     return machine.starts(with: "arm64")
 }
 
+func exec(_ command: String, args: [String]?) -> String {
+    // runs a UNIX command and returns stdout as a string
+    let proc = Process()
+    proc.launchPath = command
+    proc.arguments = args
+    let pipe = Pipe()
+    proc.standardOutput = pipe
+    proc.launch()
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    return String(data: data, encoding: String.Encoding.utf8)!
+}
+
+/// Returns true if a Python script matching scriptName is running
+func pythonScriptRunning(_ scriptName: String) -> Bool {
+    let output = exec("/bin/ps", args: ["-eo", "command="])
+    let lines = output.components(separatedBy: "\n")
+    for line in lines {
+        let part = line.components(separatedBy: " ")
+        if (part[0].contains("/MacOS/Python") || part[0].contains("python")) {
+            if part.count > 1 {
+                if (part[1] as NSString).lastPathComponent == scriptName {
+                    return true
+                }
+            }
+        }
+    }
+    return false
+}
+
+/// Returns true if there is a running executable exactly matching the name
+func executableRunning(_ name: String) -> Bool {
+    let result = exec("/usr/bin/pgrep", args: ["-x", name])
+    return !result.isEmpty
+}
+
+/// Returns true if managedsoftwareupdate is running
+func managedsoftwareupdateInstanceRunning() -> Bool {
+    // A Python version of managedsoftwareupdate might be running,
+    // or a compiled version
+    if executableRunning("managedsoftwareupdate") {
+        return true
+    }
+    if pythonScriptRunning(".managedsoftwareupdate.py") {
+        return true
+    }
+    if pythonScriptRunning("managedsoftwareupdate.py") {
+        return true
+    }
+    if pythonScriptRunning("managedsoftwareupdate") {
+        return true
+    }
+    return false
+}
+
 func checkForElCapPolicyBanner() -> Bool {
     // Returns True if we are running El Cap or later and there is
     // a loginwindow PolicyBanner in place
