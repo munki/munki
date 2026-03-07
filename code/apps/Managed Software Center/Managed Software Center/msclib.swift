3dc3c116736b8e18def344d49b8258fdd5592be3
Greg Neagle <gregneagle@mac.com>
2026-03-07 09:24:46 -0800

    Tweaks to displayed count of pending updates text

--- code/apps/Managed Software Center/Managed Software Center/msclib.swift
+++ code/apps/Managed Software Center/Managed Software Center/msclib.swift
@@ -24,15 +24,23 @@ func appleUpdateCountMessage(_ count: Int) -> String {
 
 func updateCountMessage(_ count: Int, appleUpdateCount: Int = 0) -> String {
     // Return a localized message describing the count of updates to install
-    if count == 0 {
-        if appleUpdateCount == 0 {
+    if appleUpdateCount == 0 {
+        if count == 0 {
             return NSLocalizedString("No pending updates", comment: "No Updates message") as String
+        } else if count == 1 {
+            return NSLocalizedString("1 pending update", comment: "One Update message") as String
+        } else {
+            let formatString = NSLocalizedString("%@ pending updates", comment: "Multiple Updates message")
+            return NSString(format: (formatString as NSString), String(count)) as String
         }
-        return NSLocalizedString("Managed software is up to date", comment: "No managed updates message") as String
+    }
+    // no available Apple updates to display
+    if count == 0 {
+        return NSLocalizedString("No additional pending updates", comment: "No managed updates message") as String
     } else if count == 1 {
-        return NSLocalizedString("1 pending managed update", comment: "One managed updates message") as String
+        return NSLocalizedString("1 additional pending update", comment: "One managed update message") as String
     } else {
-        let formatString = NSLocalizedString("%@ pending managed updates", comment: "Multiple managed updates message")
+        let formatString = NSLocalizedString("%@ additional pending updates", comment: "Multiple managed updates message")
         return NSString(format: (formatString as NSString), String(count)) as String
     }
 }
