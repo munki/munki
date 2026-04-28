aba738d8768fbd5ad91e6103e36671e0da94e98f
Greg Neagle <gregneagle@mac.com>
2026-04-28 08:21:30 -0700

    Bump version for future release

--- code/cli/munki/shared/version.swift
+++ code/cli/munki/shared/version.swift
@@ -18,7 +18,7 @@
 //  limitations under the License.
 
 /// one single place to define a version for CLI tools
-let CLI_TOOLS_VERSION = "7.1.0"
+let CLI_TOOLS_VERSION = "7.1.1"
 let BUILD = "<BUILD_GOES_HERE>"
 
 /// Returns version of Munki tools
