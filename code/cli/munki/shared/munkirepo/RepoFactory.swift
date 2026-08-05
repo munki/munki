0281bcf3115b5d0a65c88cbf019fd4808c1d9eff
BK <kurtz.brandon@gmail.com>
2026-08-06 00:25:16 +0200

    always look for repo plugins in /usr/local/munki/repoplugins, instead of relative to munkiimport binary

--- code/cli/munki/shared/munkirepo/RepoFactory.swift
+++ code/cli/munki/shared/munkirepo/RepoFactory.swift
@@ -54,7 +54,7 @@ private func loadRepoPlugin(at path: String) throws -> RepoPluginBuilder {
 /// Try to load a Repo plugin from our RepoPlugins directory
 func findRepoInPlugins(_ name: String, url: String) throws -> Repo? {
     let pluginName = name + ".plugin"
-    let repoPluginsDir = (Bundle.main.bundlePath as NSString).appendingPathComponent("repoplugins")
+    let repoPluginsDir = "/usr/local/munki/RepoPlugins"
     let repoPluginNames = (try? FileManager.default.contentsOfDirectory(atPath: repoPluginsDir)) ?? []
     if repoPluginNames.contains(pluginName) {
         let pluginPath = (repoPluginsDir as NSString).appendingPathComponent(pluginName)
